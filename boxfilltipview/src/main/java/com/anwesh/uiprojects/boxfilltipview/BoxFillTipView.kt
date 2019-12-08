8af972a6a87f7fb8832d2db0efb54855765bb862
Anwesh43 <anweshthecool0@gmail.com>
2019-12-08 17:27:39 +0530

    adding constants and libraries

--- boxfilltipview/src/main/java/com/anwesh/uiprojects/boxfilltipview/BoxFillTipView.kt
+++ boxfilltipview/src/main/java/com/anwesh/uiprojects/boxfilltipview/BoxFillTipView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.boxfilltipview
 /**
  * Created by anweshmishra on 08/12/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.content.Context
+
+val nodes : Int = 5
+val scGap : Float = 0.05f
+val sizeFactor : Float = 2.9f
+val strokeFactor : Int = 90
+val delay : Long = 30
+val foreColor : Int = Color.parseColor("#01579B")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val tipSizeFactor : Float = 2.9f
