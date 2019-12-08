a9819f10c42da1f9627c97175d72f50a20879dd8
Anwesh43 <anweshthecool0@gmail.com>
2019-12-08 17:35:13 +0530

    adding Canvas extension methods

--- boxfilltipview/src/main/java/com/anwesh/uiprojects/boxfilltipview/BoxFillTipView.kt
+++ boxfilltipview/src/main/java/com/anwesh/uiprojects/boxfilltipview/BoxFillTipView.kt
@@ -25,4 +25,32 @@ val tipSizeFactor : Float = 2.9f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
-fun Float.snify() : Float = Math.sin(this * Math.PI).toFloat()
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawBoxFillTip(size : Float, scale : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val tipSize : Float = size / tipSizeFactor
+    save()
+    translate(0f, -(size - tipSize) * sf)
+    paint.style = Paint.Style.FILL
+    drawRect(RectF(-tipSize / 2, -tipSize / 2, tipSize / 2, tipSize / 2), paint)
+    restore()
+    paint.style = Paint.Style.STROKE
+    drawRect(RectF(-size / 2, 0f, size / 2, size), paint)
+    paint.style = Paint.Style.FILL
+    drawRect(RectF(-size / 2, 0f, size / 2, size * scale), paint)
+}
+
+fun Canvas.drawBFTNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    paint.color = foreColor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    save()
+    translate(gap * (i + 1), h / 2)
+    drawBoxFillTip(size, scale, paint)
+    restore()
+}
